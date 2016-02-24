f98b554aa5325d667a2b3ca27ec041a576f741bf
Ben W <reccanti@gmail.com>
2016-02-23 21:12:29 -0500

    Query Rovi
    Library has been updated to query the Rovi api

--- index.js
+++ index.js
@@ -2,47 +2,74 @@
  * DEPENDENCIES
  */
 var request = require('request'),
-    qs = require('querystring');
+    qs = require('querystring'),
+    md5 = require('md5'),
+    fs = require('fs'),
+    util = require('util');
 
-var Echonest = new Function();
+var Rovi = new Function();
 
-Echonest.prototype = {
+Rovi.prototype = {
 
     /**
-     * Initializes the echonest module and sets the api keu.
+     * Initializes the rovi module and sets the api key.
      *
      * @param {String} key The echonest developer API KEY
      */
-    init: function (key) {
+    init: function (key, secret) {
         this.api_key = key;
+        this.secret = secret;
     },
 
     /**
-     * GET request to the specified echonest endpoint.
+     * GET request to the specified rovi endpoint.
      *
      * @param  {String}   endpoint The specified echonest endpoint to GET
      * @param  {Object}   params   An object that contains the GET query parameters.
      * @param  {Function} done     Callback function that takes err and result as parameters.
      */
     get: function (endpoint, params, done) {
-        if (!this.api_key || !this.api_key.trim()) {
+        
+        if ( (!this.api_key || !this.api_key.trim()) ||
+             (!this.secret || !this.secret.trim()) ) {
             return done({
                 message: 'API KEY is not set.'
             });
         }
+        var signature = genSig(this.api_key, this.secret);
+        
+        var base_url = 'http://api.rovicorp.com',
+            endpointPath = '/data/v1.1/' + endpoint + '?',
+            params = params ? qs.stringify(params) : '',
+            closingPath = '&count=0' +
+                          '&offset=0' +
+                          '&country=US' +
+                          '&language=en' +
+                          '&format=json' + 
+                          '&apikey=' + this.api_key + 
+                          '&sig=' + signature;
 
-        var base_url = 'http://developer.echonest.com',
-            path = '/api/v4/' + endpoint + '?api_key=' +  this.api_key + '&format=json',
-            params = params ? '&' + qs.stringify(params) : '';
-
-        request(base_url + path + params, function (err, response, body) {
+        var queryString = base_url + endpointPath + params + closingPath;
+        
+        request(queryString, function (err, response, body) {
             if (err || response.statusCode !== 200) {
                 return done(err || body);
             }
-
             done(null, JSON.parse(body));
         });
+        
     }
 }
 
-module.exports = new Echonest();
+/**
+ * A utility function for generating an md5 signature for
+ * use in the Rovi API.
+ */
+function genSig(apikey, secret) {
+    var curdate = new Date();
+    var gmtstring = curdate.toGMTString();
+    var utc = Date.parse(gmtstring) / 1000;
+    return  md5(apikey + secret + utc);
+}
+
+module.exports = new Rovi();
--- tests/echonest.js
+++ tests/echonest.js
@@ -5,7 +5,7 @@ var Echonest = require('../'),
 describe('Echonest module testing.', function () {
     describe('Error case in no key', function () {
         it('should return an error object when no api key is set.', function (done) {
-            Echonest.get('song/search', { artist: 'led zeppelin' }, function (err, res) {
+            Echonest.get('name/aliases', { name: 'Kanye West' }, function (err, res) {
                 expect(res).to.be.an('undefined');
                 expect(err.message).to.equal('API KEY is not set.');
 
@@ -16,15 +16,14 @@ describe('Echonest module testing.', function () {
 
     describe('Echonest api key is set: ', function () {
         before(function (done) {
-            Echonest.init(config.api_key);
+            Echonest.init(config.api_key, config.secret);
             done();
         });
 
         describe('Error case in no valid endpoint', function () {
             it('should return an error when an invalid echonest endpoint is used.', function (done) {
-                Echonest.get('zong/zearch', { artist: 'led zeppelin' }, function (err, res) {
-                    expect(err).to.equal('<html><title>404: Not Found</title><body>404: Not Found</body></html>');
-
+                Echonest.get('pane/paliases', { artist: 'Kanye West' }, function (err, res) {
+                    expect(err).to.equal('<h1>596 Service Not Found</h1>');
                     done();
                 });
             });
@@ -32,9 +31,9 @@ describe('Echonest module testing.', function () {
 
         describe('Success case', function () {
             it('should return a results object.', function (done) {
-                Echonest.get('song/search', { artist: 'led zeppelin' }, function (err, res) {
-                    expect(res.response.status.message).to.equal('Success');
-                    expect(res.response.songs).to.be.an('array');
+                Echonest.get('name/aliases', { name: 'Kanye West' }, function (err, res) {
+                    expect(res.status).to.equal('ok');
+                    expect(res.aliases).to.be.an('array');
 
                     done();
                 });
